964d17e6cf0ef7819612d4e91660965c49d14096
Fillipe143 <fillipedev.contato@gmail.com>
2023-11-12 11:18:34 -0300

    HelloAsm

---
+++ hello/hello.asm
@@ -0,0 +1,18 @@
+section .data
+	msg 	db "Hello World!", 0xA 
+	msg_len equ $- msg
+
+section .text
+
+global _start
+_start:
+	mov eax, 0x4
+	mov ebx, 0x1
+	mov ecx, msg
+	mov edx, msg_len
+	int 0x80
+	
+	mov eax, 0x1
+	mov ebx, 0x0
+	int 0x80
+
